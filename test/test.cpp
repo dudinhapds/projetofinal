09e051e93fccc24771c9c3e9c8893e67b56ee37d
marccellaa <105751333+marccellaa@users.noreply.github.com>
2023-11-28 19:54:54 -0300

    Update test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -1,1 +1,14 @@
+#include <GerenciadorEstadoApp.hpp>
+#include <AutenticadorUsuario.hpp>
 
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+TEST_CASE("Testando as chaves") {
+    Chave chave1("senha");
+    Chave chave2("senha");
+    Chave chave3("outra senha");
+
+    CHECK(chave1 == chave2);
+    CHECK(chave1 != chave3);
+}
