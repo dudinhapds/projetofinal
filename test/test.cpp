733d092868e18edfcf5a366c6cbbaada08d8fbf1
marccellaa <105751333+marccellaa@users.noreply.github.com>
2023-11-28 20:07:43 -0300

    Update test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -12,3 +12,37 @@ TEST_CASE("Testando as chaves") {
     CHECK(chave1 == chave2);
     CHECK(chave1 != chave3);
 }
+
+TEST_CASE("Testando a serialização") {
+
+    #define OUTPUT  "output/test1.tmp"
+
+    Chave chave("senha"), chave2;
+    std::uint64_t valor, valor2;
+
+    {
+        std::FILE *arquivo = std::fopen(OUTPUT, "wb");
+        REQUIRE_NE(arquivo, nullptr);
+        OutputStream ostream(arquivo, chave);
+        valor = 0x123456789abcdef0;
+
+        CHECK_EQ(ostream.write(&chave, sizeof(chave)), sizeof(chave));
+        CHECK_EQ(ostream.write(&valor, sizeof(valor)), sizeof(valor));
+    }
+    
+    {
+        std::FILE *arquivo = std::fopen(OUTPUT, "rb");
+        REQUIRE_NE(arquivo, nullptr);
+        InputStream istream(arquivo, chave);
+        
+        CHECK_EQ(istream.read(&chave2, sizeof(chave2)), sizeof(chave2));
+        CHECK_EQ(istream.read(&valor2, sizeof(valor2)), sizeof(valor2));
+    }
+
+    CHECK(chave == chave2);
+    CHECK_EQ(valor, valor2);
+
+    std::remove(OUTPUT);
+    
+    #undef OUTPUT
+}
