7c93b30bc5d7e52ea7cc5c49f3dcd574467daefe
Sabrinaseixas <145785182+Sabrinaseixas@users.noreply.github.com>
2023-11-27 21:19:28 -0300

    Update AutenticadorUsuario.hpp

--- include/AutenticadorUsuario.hpp
+++ include/AutenticadorUsuario.hpp
@@ -1,1 +1,18 @@
+#ifndef AUTENTICADORUSUARIO_HPP
+#define AUTENTICADORUSUARIO_HPP
 
+#include <SerializadorUsuario.hpp>
+#include <optional>
+
+class AutenticadorUsuario {
+public:
+    static std::optional<SerializadorUsuario> autenticar(
+        std::string const &nome, std::string const &senha
+    );
+    
+    static std::optional<SerializadorUsuario> criar(
+        std::string const &nome, std::string const &senha
+    );
+};
+
+#endif // AUTENTICADORUSUARIO_HPP
