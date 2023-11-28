b8506df792ee8304007712c50361f6747c3c0649
Sabrinaseixas <145785182+Sabrinaseixas@users.noreply.github.com>
2023-11-27 21:27:43 -0300

    Update Usuario.hpp

--- include/Usuario.hpp
+++ include/Usuario.hpp
@@ -1,1 +1,21 @@
+#ifndef USUARIO_HPP
+#define USUARIO_HPP
 
+#include <ListaTarefasCompromissos.hpp>
+#include <PreferenciasLista.hpp>
+#include <string>
+
+class Usuario {
+public:
+    Usuario(std::string const &nome, ListaTarefasCompromissos const &lista);
+
+    std::string const &getNome() const;
+    ListaTarefasCompromissos &getListaTarefas();
+    PreferenciasLista &getPreferencias();
+private:
+    std::string nome;
+    ListaTarefasCompromissos listaTarefas;
+    PreferenciasLista preferencias;
+};
+
+#endif // USUARIO_HPP
