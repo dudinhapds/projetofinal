aab628cfae0f39b399672fb091ed34e8816a3f54
Sabrinaseixas <145785182+Sabrinaseixas@users.noreply.github.com>
2023-11-27 21:25:34 -0300

    Update SerializadorUsuario.hpp

--- include/SerializadorUsuario.hpp
+++ include/SerializadorUsuario.hpp
@@ -1,1 +1,23 @@
+#ifndef SERIALIZADORUSUARIO_HPP
+#define SERIALIZADORUSUARIO_HPP
 
+#include <ListaTarefasCompromissos.hpp>
+
+#include <string>
+
+class ListaTarefasCompromissos;
+
+class SerializadorUsuario {
+public:
+    SerializadorUsuario(std::string const &nome_arquivo, Chave chave);
+    ~SerializadorUsuario();
+
+    void serializar(ListaTarefasCompromissos const &lista);
+    ListaTarefasCompromissos deserializar();
+    
+private:
+    std::string nome_arquivo;
+    Chave chave;
+};
+
+#endif // SERIALIZADORUSUARIO_HPP
