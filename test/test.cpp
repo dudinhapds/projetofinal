83d1174e4a1d27921e775f63a9ecfa89482d6c47
marccellaa <105751333+marccellaa@users.noreply.github.com>
2023-11-29 06:18:20 -0300

    Update test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -265,7 +265,37 @@ TEST_CASE("Testando a serialização de uma tarefa") {
         CHECK_EQ(tarefa1.getPrioridade(), tarefa2.getPrioridade());
         CHECK_EQ(tarefa1.getConcluida(), tarefa2.getConcluida());
     }
-    
+     SUBCASE("Exemplo de tarefa 2") {
+        std::tm tm {
+            .tm_sec = 0,
+            .tm_min = 30,
+            .tm_hour = 12,
+            .tm_mday = 23,
+            .tm_mon = 10, // Nov
+            .tm_year = 2023 - 1900,
+        };
+        std::time_t timestamp = std::mktime(&tm);
+        
+        TarefaCompromisso tarefa1(
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do "
+            "eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut "
+            "enim ad minim veniam, quis nostrud exercitation ullamco laboris "
+            "nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor "
+            "in reprehenderit in voluptate velit esse cillum dolore eu fugiat "
+            "nulla pariatur. Excepteur sint occaecat cupidatat non proident, "
+            "sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            timestamp, TarefaCompromisso::P_BAIXA, false
+        );
+        TarefaCompromisso tarefa2;
+
+        testarTarefa(tarefa1, &tarefa2, OUTPUT, chave);
+
+        CHECK_EQ(tarefa1.getDescricao(), tarefa2.getDescricao());
+        CHECK_EQ(tarefa1.getTimestamp(), tarefa2.getTimestamp());
+        CHECK_EQ(tarefa1.getPrioridade(), tarefa2.getPrioridade());
+        CHECK_EQ(tarefa1.getConcluida(), tarefa2.getConcluida());
+    }
+
     std::remove(OUTPUT);
     
     #undef OUTPUT
