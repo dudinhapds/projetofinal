0c1f285583364335bb2b5cd6e797e1e3703c956b
Sabrinaseixas <145785182+Sabrinaseixas@users.noreply.github.com>
2023-11-29 21:12:11 -0300

    Comentários Doxygen SerializadorUsuario.hpp

--- include/SerializadorUsuario.hpp
+++ include/SerializadorUsuario.hpp
@@ -1,22 +1,48 @@
 #ifndef SERIALIZADORUSUARIO_HPP
 #define SERIALIZADORUSUARIO_HPP
 
-#include <ListaTarefasCompromissos.hpp>
+#include <Usuario.hpp>
 
 #include <string>
 
-class ListaTarefasCompromissos;
-
+/**
+ * @brief Classe serializadora de usuário.
+ * 
+ * A partir desta classe, é possível acessar e manipular os dados salvos no
+ * arquivo do usuário.
+ */
 class SerializadorUsuario {
 public:
+    /**
+     * @brief Construtor da classe SerializadorUsuario.
+     * @param nome_arquivo Nome do arquivo do usuário.
+     * @param chave Chave gerada com a senha do usuário.
+     */
     SerializadorUsuario(std::string const &nome_arquivo, Chave chave);
+    /**
+     * @brief Destrutor da classe SerializadorUsuario.
+     */
     ~SerializadorUsuario();
 
-    void serializar(ListaTarefasCompromissos const &lista);
-    ListaTarefasCompromissos deserializar();
+    /**
+     * @brief Serializa os dados do usuário.
+     * @param usuario Referência ao usuário.
+     */
+    void serializar(Usuario const &usuario);
+    /**
+     * @brief Deserializa os dados do usuário.
+     * @param usuario Ponteiro para o usuário.
+     */
+    void deserializar(Usuario *usuario);
     
 private:
+    /**
+     * @brief Nome do arquivo do usuário.
+     */
     std::string nome_arquivo;
+    /**
+     * @brief Chave gerada com a senha do usuário.
+     */
     Chave chave;
 };
 
