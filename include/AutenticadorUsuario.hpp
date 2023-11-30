ad428179720fcac9a82109d9792c55c2dec6ae93
Sabrinaseixas <145785182+Sabrinaseixas@users.noreply.github.com>
2023-11-29 21:04:13 -0300

    Adição coments Doxygen pt2

--- include/AutenticadorUsuario.hpp
+++ include/AutenticadorUsuario.hpp
@@ -24,4 +24,22 @@ public:
     static std::optional<SerializadorUsuario> autenticar(
         std::string const &nome, std::string const &senha
     );
-    
+  
+    /**
+     * @brief Tenta criar um novo usuário.
+     * 
+     * Esta função tenta criar um novo usuário, isto é, verificar se o usuário
+     * não existe e, se não existir, criar um novo usuário com o nome e senha
+     * especificados.
+     * 
+     * @param nome Nome do usuário
+     * @param senha Senha do usuário
+     * @return Retorna um `SerializadorUsuario` associado ao usuário criado,
+     * caso a criação tenha sido bem-sucedida.
+     */
+    static std::optional<SerializadorUsuario> criar(
+        std::string const &nome, std::string const &senha
+    );
+};
+
+#endif // AUTENTICADORUSUARIO_HPP    
