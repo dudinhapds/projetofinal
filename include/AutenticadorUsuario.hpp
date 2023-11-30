d82b6531d7bee6c6b501b19eac707eec87e1fc89
Sabrinaseixas <145785182+Sabrinaseixas@users.noreply.github.com>
2023-11-29 21:03:36 -0300

    Adição dos comentários Doxygen

--- include/AutenticadorUsuario.hpp
+++ include/AutenticadorUsuario.hpp
@@ -4,15 +4,24 @@
 #include <SerializadorUsuario.hpp>
 #include <optional>
 
+/**
+ * @brief Classe estática que lida com a autenticação e criação de usuários.
+ */
 class AutenticadorUsuario {
 public:
+    /**
+     * @brief Tenta fazer o login de um usuário.
+     * 
+     * Esta função tenta fazer o login de um usuário, isto é, verificar se
+     * o usuário existe e se a senha está correta. Se sim, retorna um
+     * `SerializadorUsuario` associado ao usuário logado.
+     * 
+     * @param nome Nome do usuário
+     * @param senha Senha do usuário
+     * @return Retorna um SerializadorUsuario associado ao usuário logado,
+     * caso o login tenha sido bem-sucedido.
+     */
     static std::optional<SerializadorUsuario> autenticar(
         std::string const &nome, std::string const &senha
     );
     
-    static std::optional<SerializadorUsuario> criar(
-        std::string const &nome, std::string const &senha
-    );
-};
-
-#endif // AUTENTICADORUSUARIO_HPP
